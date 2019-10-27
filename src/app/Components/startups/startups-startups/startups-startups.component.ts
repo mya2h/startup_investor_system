b2f2ac8db4daf0689564b5fc64851524d2f96fdc
Kalkidan Mesfin <mesfinkalkidan@gmail.com>
2019-10-27 21:23:28 +0300

    startup all projects

--- src/app/Components/startups/startups-startups/startups-startups.component.ts
+++ src/app/Components/startups/startups-startups/startups-startups.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import {LoginService} from '../../../Services/login.service'
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import {StartupProfileService } from '../../../Services/startup-profile.service';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig} from '@angular/material/dialog';
+import {ViewprojectComponent} from '../viewproject/viewproject.component'
+import { from } from 'rxjs';
+import {project} from '../../../Model/project'
+import { DataSource } from '@angular/cdk/table';
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 @Component({
   selector: 'app-startups-startups',
   templateUrl: './startups-startups.component.html',
   styleUrls: ['./startups-startups.component.css']
 })
-export class StartupsStartupsComponent implements OnInit {
+export class StartupsStartupsComponent implements OnInit ,AfterViewInit{
+  public displayedColumns = ['name', 'sector', 'industry_type',
+   'current_capital','maximum_fund','current_status',
+   'details', 'update', 'delete'];
+public dataSource = new MatTableDataSource<project>();
 
-  constructor() { }
+  @ViewChild(MatSort,{static: false}) sort: MatSort;
+  @ViewChild(MatPaginator,{static: false}) paginator: MatPaginator;
+
+  constructor(private profile:StartupProfileService,private dialog: MatDialog) { }
+  animal: string;
+  name: string;
   ngOnInit() {
+    this.getAllProjects();
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  getAllProjects(){
+    this.profile.getProject().subscribe(res=>{
+      this.dataSource.data = res as project[];
+      console.log(this.dataSource.data);
+     })
+  }
    
+  public redirectToDetails = (id: string) => {
+    
+  }
+ 
+  public redirectToUpdate = (id: string) => {
+    
+  }
+ 
+  public redirectToDelete = (id: string) => {
+    
+  }
+	public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(ViewprojectComponent, {
+      width: '250px',
+      data:this.dataSource.data
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+        
+    });
+  }
   }
 
-}
