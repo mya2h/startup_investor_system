d06ae0a8bbb3e08af475b493875fe80d01644a47
Kalkidan Mesfin <mesfinkalkidan@gmail.com>
2019-10-27 21:31:54 +0300

    view project detail

---
+++ src/app/Components/startups/viewproject/viewproject.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit,Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {StartupsStartupsComponent} from '../startups-startups/startups-startups.component';
+import {DialogData} from '../startups-startups/startups-startups.component';
+
+@Component({
+  selector: 'app-viewproject',
+  templateUrl: './viewproject.component.html',
+  styleUrls: ['./viewproject.component.css']
+})
+export class ViewprojectComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<StartupsStartupsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  
+  ngOnInit() {
+  
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
